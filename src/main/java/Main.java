189ca53514b648a68946de0070762b273048806d
Adrian <adrian.essig@outlook.com>
2022-03-13 18:33:55 +0100

    Added RMI Registry (locateregistry and naming rebind)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,19 @@ import application.CustomerServiceImpl;
 import application.api.CustomerService;
 import application.dto.CustomerDTO;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws RemoteException {
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+
+        LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+
+        Naming.rebind("rmi://localhost/getCustomersByName", new CustomerServiceImpl());
 
         CustomerService customerService = new CustomerServiceImpl();
         List<CustomerDTO> customerList = customerService.getCustomerByName("Jake");
