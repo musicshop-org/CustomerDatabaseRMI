1581b1fbca558221e1a2c2ceee74bbb7dbdfd87e
schlaumeierisch <matthias.meier97@hotmail.com>
2022-06-20 16:24:57 +0200

    optimized imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,18 @@
-import sharedrmi.application.api.CustomerService;
-import sharedrmi.application.dto.CustomerDTO;
-
 import application.CustomerServiceImpl;
 
-import javax.naming.NoPermissionException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws RemoteException, MalformedURLException, InterruptedException, NoPermissionException {
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
 
         System.setProperty("java.rmi.server.hostname","10.0.40.163");
         LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
 
         Naming.rebind("rmi://localhost/CustomerService", new CustomerServiceImpl());
 
-//        CustomerService customerService = new CustomerServiceImpl();
-//        List<CustomerDTO> customerList = customerService.findCustomersByName("Henry");
-
-//        for (CustomerDTO c:customerList) {
-//            System.out.println(c.getFirstName() + " " + c.getLastName() + " " +  c.getEmail());
-//            System.out.println(c.getAddress().getAddressCountry());
-//            System.out.println(c.getAddress().getAddressLocality());
-//            System.out.println(c.getAddress().getPostalCode());
-//            System.out.println(c.getAddress().getStreetAddress());
-//        }
     }
 }
